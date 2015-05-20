d3ea18aee7a4904f0ec77b920e868e0ee7ce5554
cwessel188 <wesselcp@plu.edu>
2015-05-19 22:59:05 -0700

    Added seperate project containing the old client side version

---
+++ ClientsideApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ClientsideApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ClientsideApp/Hubs/BrainEatersHub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Diagnostics;
+using BrainEaters.Models;
+
+namespace BrainEaters.Hubs
+{
+    public class BrainEatersHub : Hub
+    {
+        private BrainEatersGame _game { get; set; }
+        public BrainEatersHub()
+        {
+            _game = BrainEatersGame.Instance;
+        }
+        public void SendMessage(string message) // server message
+        {
+            //SignalRUpdateGame.DoSomething()//C# methods from C# class
+            Clients.All.SendMessage(message); // client message
+        }
+
+        public void KeyPressed(int keyCode) // server message
+        {
+            GameEngine.MovePlayer(keyCode);
+            Clients.Caller.UpdateGame(_game);
+        }
+
+        public void RequestGameState()
+        {
+            Clients.Caller.UpdateGame(_game);
+        }
+
+        public void AddPlayer(string name)
+        {
+            GameEngine.AddPlayer(name);
+            // add player to upper left of game
+            Clients.Caller.UpdateGame(_game);
+        }
+    }
+}

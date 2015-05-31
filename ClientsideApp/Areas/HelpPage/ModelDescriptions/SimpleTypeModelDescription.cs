fd1dece015f690b2884c662b16cb509dd4bb1842
cwessel188 <wesselcp@plu.edu>
2015-05-30 22:18:35 -0700

    deleted extra projects

--- ClientsideApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ClientsideApp.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- ClientsideBrainEaters/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClientsideBrainEaters.MvcApplication" Language="C#" %>

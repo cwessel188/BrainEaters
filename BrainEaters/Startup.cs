69f01105e9525700defb193d45caa1d3b204c289
cwessel188 <wesselcp@plu.edu>
2015-06-11 17:44:17 -0700

    fixed owin startup problem

--- BrainEaters/Startup.cs
+++ BrainEaters/Startup.cs
@@ -12,9 +12,9 @@ namespace BrainEaters
     {
         public void Configuration(IAppBuilder app)
         {
+       //     ConfigureAuth(app);
             // add SignalR mapping
             app.MapSignalR();
-            ConfigureAuth(app);
         }
     }
 }

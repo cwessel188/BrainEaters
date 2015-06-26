00638cb08a1dfa91d62c339784803ed03c6ca767
cwessel188 <wesselcp@plu.edu>
2015-06-26 11:24:53 -0700

    chat scrolls

--- BrainEaters/Scripts/braineaters/client.js
+++ BrainEaters/Scripts/braineaters/client.js
@@ -78,13 +78,13 @@ var drawGame = function (data) {
                     context.fillRect(data.CellWidth * x, data.CellWidth * y, data.CellWidth, data.CellWidth);
                     break;
                 case ("0"): // 0 is falsey and must be handled seperately
-                    // drawPlayerImage(0, x, y, data.CellWidth);
-                    drawPlayerColor(data, 0, x, y, data.CellWidth)
+                    // drawPlayerAsImage(0, x, y, data.CellWidth);
+                    drawPlayerAsColor(data, 0, x, y, data.CellWidth)
                     break;
                 default:
                     if (parseInt(data.GameArray[x][y], 10)) { // it's a number
-                        // drawPlayerImage(parseInt(data.GameArray[x][y], 10), x, y, data.CellWidth);
-                        drawPlayerColor(data, parseInt(data.GameArray[x][y], 10), x, y, data.CellWidth);
+                        // drawPlayerAsImage(parseInt(data.GameArray[x][y], 10), x, y, data.CellWidth);
+                        drawPlayerAsColor(data, parseInt(data.GameArray[x][y], 10), x, y, data.CellWidth);
                     }
                     else {
                         console.log("Error in drawPlayer: char not recognized" + data.GameArray[x][y]);
@@ -94,14 +94,14 @@ var drawGame = function (data) {
     }      // for y
 };         // draw game
 
-var drawPlayerImage = function (plrNumber, x, y, cellWidth) {
+var drawPlayerAsImage = function (plrNumber, x, y, cellWidth) {
     context.drawImage(
         window["player" + plrNumber].img,
         cellWidth * x + IMG_PADDING, cellWidth * y + IMG_PADDING,
         IMG_SIZE, IMG_SIZE);
 };
 
-var drawPlayerColor = function (data, plrNumber, x, y, cellWidth) {
+var drawPlayerAsColor = function (data, plrNumber, x, y, cellWidth) {
     context.fillStyle = data.Players[plrNumber].Color;
     context.fillRect(cellWidth * x + IMG_PADDING, cellWidth * y + IMG_PADDING, // coors
         IMG_SIZE, IMG_SIZE); // size
